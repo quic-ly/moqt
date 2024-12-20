4132cb5fb7f421f594a8051123c9fed50209df09
yngrtc <y@ngr.tc>
2024-12-20 13:58:54 -0800

    add moqt_messages_test

--- moqt/src/moqt_messages.rs
+++ moqt/src/moqt_messages.rs
@@ -290,7 +290,7 @@ impl fmt::Display for FullTrackName {
             bits.push("\"".to_owned() + raw_bit + "\"");
         }
 
-        write!(f, "{{{}}}", bits.join(","))
+        write!(f, "{{{}}}", bits.join(", "))
     }
 }
 
@@ -777,4 +777,57 @@ pub struct MoqtObjectAck {
     object_id: u64,
     /// Positive if the object has been received before the deadline.
     delta_from_deadline: Duration,
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_full_track_name_constructors() {
+        let name1 = FullTrackName::new_with_namespace_and_name("foo", "bar");
+        let list = vec!["foo".to_string(), "bar".to_string()];
+        let name2 = FullTrackName::new_with_elements(list);
+        assert_eq!(name1, name2);
+        //assert_eq!(HashOf(name1), HashOf(name2));
+    }
+
+    #[test]
+    fn test_full_track_name_order() {
+        let name1 = FullTrackName::new_with_elements(vec!["a".to_string(), "b".to_string()]);
+        let name2 = FullTrackName::new_with_elements(vec![
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+        ]);
+        let name3 = FullTrackName::new_with_elements(vec!["b".to_string(), "a".to_string()]);
+        assert!(name1 < name2);
+        assert!(name2 < name3);
+        assert!(name1 < name3);
+    }
+
+    #[test]
+    fn test_full_track_name_in_namespace() {
+        let name1 = FullTrackName::new_with_elements(vec!["a".to_string(), "b".to_string()]);
+        let name2 = FullTrackName::new_with_elements(vec![
+            "a".to_string(),
+            "b".to_string(),
+            "c".to_string(),
+        ]);
+        let name3 = FullTrackName::new_with_elements(vec!["b".to_string(), "a".to_string()]);
+
+        assert!(name2.in_namespace(&name1));
+        assert!(!name1.in_namespace(&name2));
+        assert!(name1.in_namespace(&name1));
+        assert!(!name2.in_namespace(&name3));
+    }
+
+    #[test]
+    fn test_full_track_name_to_string() {
+        let name1 = FullTrackName::new_with_elements(vec!["a".to_string(), "b".to_string()]);
+        assert_eq!(name1.to_string(), r#"{"a", "b"}"#);
+
+        //TODO: let name2 = FullTrackName::new_with_elements(vec!["\xff".to_string(), "\x61".to_string()]);
+        // assert_eq!(name2.to_string(), r#"{"\xff", "a"}"#);
+    }
 }
